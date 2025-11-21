842743a673dcebab66d30dee8f765d661e1f180e
sAngello31 <bravoangello31@gmail.com>
2025-11-21 01:03:27 -0500

    Implement REPL functionality with welcome message and command handling

--- src/shell/repl.rs
+++ src/shell/repl.rs
@@ -1,3 +1,22 @@
+use std::io::{self, Write};
+
 pub fn run(){
-    println!("REPL is running");
+    println!("Welcome to the Toy Rust Shell!");
+
+    loop{
+        print!("> ");
+        std::io::stdout().flush().expect("Error flusing stdout");
+        let mut input: String = String::new();
+        if io::stdin().read_line(&mut input).is_err(){
+            continue;
+        }
+
+        let command = input.trim();
+        if command == "exit"{
+            break;
+        }
+
+        println!("{}", command);
+
+    }
 }
